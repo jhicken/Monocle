fe08d125559c71ad537369677fee590b57bdd1d1
Jeff Hicken <jhicken@gmail.com>
2014-12-08 14:10:22 -0700

    Change place saver to use localstrorage

--- src/controls/placesaver.js
+++ src/controls/placesaver.js
@@ -1,100 +1,81 @@
 Monocle.Controls.PlaceSaver = function (bookId) {
 
-  var API = { constructor: Monocle.Controls.PlaceSaver }
-  var k = API.constants = API.constructor;
-  var p = API.properties = {}
+    var API = { constructor: Monocle.Controls.PlaceSaver }
+    var k = API.constants = API.constructor;
+    var p = API.properties = {}
 
 
-  function initialize() {
-    applyToBook(bookId);
-  }
-
-
-  function assignToReader(reader) {
-    p.reader = reader;
-    p.reader.listen('monocle:turn', savePlaceToCookie);
-  }
-
-
-  function applyToBook(bookId) {
-    p.bkTitle = bookId.toLowerCase().replace(/[^a-z0-9]/g, '');
-    p.prefix = k.COOKIE_NAMESPACE + p.bkTitle + ".";
-  }
-
-
-  function setCookie(key, value, days) {
-    var expires = "";
-    if (days) {
-      var d = new Date();
-      d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires="+d.toGMTString();
+    function initialize() {
+        applyToBook(bookId);
     }
-    var path = "; path=/";
-    document.cookie = p.prefix + key + "=" + value + expires + path;
-    return value;
-  }
 
 
-  function getCookie(key) {
-    if (!document.cookie) {
-      return null;
+    function assignToReader(reader) {
+        p.reader = reader;
+        p.reader.listen('monocle:turn', savePlaceToCookie);
     }
-    var regex = new RegExp(p.prefix + key + "=(.+?)(;|$)");
-    var matches = document.cookie.match(regex);
-    if (matches) {
-      return matches[1];
-    } else {
-      return null;
+
+
+    function applyToBook(bookId) {
+        p.bkTitle = bookId.toLowerCase().replace(/[^a-z0-9]/g, '');
+        p.prefix = k.NAMESPACE + p.bkTitle + ".";
     }
-  }
 
 
-  function savePlaceToCookie() {
-    var place = p.reader.getPlace();
-    setCookie(
-      "component",
-      encodeURIComponent(place.componentId()),
-      k.COOKIE_EXPIRES_IN_DAYS
-    );
-    setCookie(
-      "percent",
-      place.percentageThrough(),
-      k.COOKIE_EXPIRES_IN_DAYS
-    );
-  }
-
-
-  function savedPlace() {
-    var locus = {
-      componentId: getCookie('component'),
-      percent: getCookie('percent')
+    function setLocalStorage(key, value) {
+        localStorage[p.prefix + key] = value;
+        return value;
     }
-    if (locus.componentId && locus.percent) {
-      locus.componentId = decodeURIComponent(locus.componentId);
-      locus.percent = parseFloat(locus.percent);
-      return locus;
-    } else {
-      return null;
+
+
+    function getLocalStorage(key) {
+        return localStorage[p.prefix + key];
     }
-  }
 
 
-  function restorePlace() {
-    var locus = savedPlace();
-    if (locus) {
-      p.reader.moveTo(locus);
+    function savePlaceToCookie() {
+        var place = p.reader.getPlace();
+        setLocalStorage(
+            "component",
+            encodeURIComponent(place.componentId())
+        );
+        setLocalStorage(
+            "percent",
+            place.percentageThrough()
+        );
     }
-  }
 
 
-  API.assignToReader = assignToReader;
-  API.savedPlace = savedPlace;
-  API.restorePlace = restorePlace;
+    function savedPlace() {
+        var locus = {
+            componentId: getLocalStorage('component'),
+            percent: getLocalStorage('percent')
+        }
+        if (locus.componentId && locus.percent) {
+            locus.componentId = decodeURIComponent(locus.componentId);
+            locus.percent = parseFloat(locus.percent);
+            return locus;
+        } else {
+            return null;
+        }
+    }
 
-  initialize();
 
-  return API;
+    function restorePlace() {
+        var locus = savedPlace();
+        if (locus) {
+            p.reader.moveTo(locus);
+        }
+    }
+
+
+    API.assignToReader = assignToReader;
+    API.savedPlace = savedPlace;
+    API.restorePlace = restorePlace;
+
+    initialize();
+
+    return API;
 }
 
-Monocle.Controls.PlaceSaver.COOKIE_NAMESPACE = "monocle.controls.placesaver.";
-Monocle.Controls.PlaceSaver.COOKIE_EXPIRES_IN_DAYS = 7; // Set to 0 for session-based expiry.
+Monocle.Controls.PlaceSaver.NAMESPACE = "monocle.controls.placesaver.";
